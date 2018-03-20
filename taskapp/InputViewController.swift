854645e7bc3b99f8a9f5bdf36a3db324160e338c
世古直輝 <seko@sekonaokinomacbook-puro.local>
2018-03-21 08:26:21 +0900

    InputViewControllerにパーツを設置した

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class InputViewController: UIViewController {
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
